5a4762a3ff091b7ab534081fc21e60f17b6dcf64
dmitriy krasnov <dmitriy.krasnov@travelline.ru>
2025-04-03 13:15:25 +0300

    Добавляет show в offer контроллер

--- src/shared/modules/offer/offer.controller.ts
+++ src/shared/modules/offer/offer.controller.ts
@@ -1,10 +1,12 @@
 import { inject, injectable } from 'inversify';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+
+import { OfferService } from './offer-service.interface.js';
+import { ParamOfferId } from './type/param-offerid.type.js';
 import { COMPONENT_MAP } from '../../types/index.js';
 import { BaseController, HttpError, HttpMethod } from '../../libs/rest/index.js';
 import { Logger } from '../../libs/logger/index.js';
-import { OfferService } from './offer-service.interface.js';
 import { fillDTO } from '../../helpers/index.js';
 import { OfferRdo } from './rdo/offer.rdo.js';
 import { CreateOfferRequest } from './create-offer-request.type.js';
@@ -20,11 +22,12 @@ export class OfferController extends BaseController {
 
     this.logger.info('Register routes for OfferController');
 
-    this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.get });
+    this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
     this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
+    this.addRoute({ path: '/:offerId', method: HttpMethod.Get, handler: this.show });
   }
 
-  public async get(_req: Request, res: Response): Promise<void> {
+  public async index(_req: Request, res: Response): Promise<void> {
     const offers = await this.offerService.find();
 
     if (!offers.length) {
@@ -41,5 +44,20 @@ export class OfferController extends BaseController {
   public async create({ body }: CreateOfferRequest, res: Response): Promise<void> {
     const result = await this.offerService.create(body);
     this.created(res, fillDTO(OfferRdo, result));
+  }
+
+  public async show({ params }: Request<ParamOfferId>, res: Response): Promise<void> {
+    const { offerId } = params;
+    const offer = await this.offerService.findById(offerId);
+
+    if (! offer) {
+      throw new HttpError(
+        StatusCodes.NOT_FOUND,
+        `Offer with id ${offerId} not found.`,
+        'OfferController'
+      );
+    }
+
+    this.ok(res, offer);
   }
 }
